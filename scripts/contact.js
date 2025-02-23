bcae60fd73b265bcee453e5a3fe973a35c3b8147
Quoc Pham <quocpham2110@gmail.com>
2025-02-23 18:35:55 -0500

    assignment 2

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,124 +1,98 @@
-"use strict";
+"use strict"
 
 /**
- * Represents a Contact with a name, contact number and email address
+ * Represents a Contact with a name, email address, subject and message
  */
-
-(function (core) {
-
-    class Contact {
-        /**
-         * Constructs a new Contact instance
-         * @param fullName
-         * @param contactNumber
-         * @param emailAddress
-         */
-        constructor(fullName = "", contactNumber = "", emailAddress = "") {
-            this._fullName = fullName;
-            this._contactNumber = contactNumber;
-            this._emailAddress = emailAddress;
-        }
-
-        /**
-         * Get the full name of the contact
-         * @returns {string}
-         */
-        get fullName() {
-            return this._fullName;
-        }
-
-        /**
-         * Set the full name of the contact. Validates input to ensure it's a non-empty string
-         * @param fullName
-         */
-        set fullName(fullName) {
-            if (typeof fullName !== "string" || fullName.trim() === "") {
-                throw new Error("Invalid full name: must be non-empty string.");
-            }
-            this._fullName = fullName;
-        }
-
-        /**
-         * Get the contact number of the contact
-         * @returns {string}
-         */
-        get contactNumber() {
-            return this._contactNumber;
-        }
-
-        /**
-         * Set the contact number of the contact. Validates input to ensure it's a 10-digit number
-         * @param contactNumber
-         */
-        set contactNumber(contactNumber) {
-            const contactPattern = new RegExp(/^\d{3}-\d{3}-\d{4}$/); // 437-888-8888
-            if (!contactPattern.test(contactNumber)) {
-                throw new Error("Invalid contact number: must be a 10-digit number.");
-            }
-            this._contactNumber = contactNumber;
-
-        }
-
-        /**
-         * Get the email address of the contact
-         * @returns {string}
-         */
-        get emailAddress() {
-            return this._emailAddress;
-        }
-
-        /**
-         * Set the email address of the contact. Validates input to ensure it's a standard email format
-         * @param emailAddress
-         */
-        set emailAddress(emailAddress) {
-            // const emailPattern = new RegExp(/^\w+@\w{3,}\.\w{2,4}$/);
-            const emailPattern = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
-            if (!emailPattern.test(emailAddress)) {
-                throw new Error("Invalid email address: must be non-empty string.");
-            }
-            this._emailAddress = emailAddress;
-        }
-
-        /**
-         * Converts the contact details into human-readable string
-         * @returns {string}
-         */
-        toString() {
-            return `Full Name : ${this._fullName}\n
-        Contact Number: ${this._contactNumber}\n
-        Email Address: ${this._emailAddress}`
-        }
-
-        /**
-         * Serializes the contact details into a string format suitable for storage
-         * @returns {string|null}
-         */
-        serialize() {
-            if (!this._fullName || !this._contactNumber || !this._emailAddress) {
-                console.error("One or more of the contact properties are missing or invalid")
-                return null;
-            }
-
-            return `${this._fullName},${this._contactNumber},${this._emailAddress}`;
-        }
-
-        /**
-         * Deserializes a string (comma-delimited) of contact details and update properties
-         * @param data
-         */
-        deserialize(data) {
-            if (typeof data !== "string" || data.split(",").length !== 3) {
-                console.error("Invalid data format for deserializing data.");
-                return;
-            }
-
-            const [fullName, contactNumber, emailAddress] = data.split(",");
-            this._fullName = fullName;
-            this._contactNumber = contactNumber;
-            this._emailAddress = emailAddress;
-        }
+class Contact {
+    /**
+     * Constructs a new Contact instance
+     * @param fullName
+     * @param emailAddress
+     * @param subject
+     * @param message
+     */
+    constructor(fullName = "", emailAddress = "", subject = "", message = "") {
+        this.fullName = fullName;
+        this.emailAddress = emailAddress;
+        this.subject = subject;
+        this.message = message;
     }
 
-    core.Contact = Contact;
-})(core || (core = {}));
+    /**
+     * Get the full name of the contact
+     * @returns {string}
+     */
+    get fullName() {
+        return this._fullName;
+    }
+
+    /**
+     * Set the full name of the contact. Validates input to ensure it's a non-empty string
+     * @param fullName
+     */
+    set fullName(fullName) {
+        if (typeof fullName !== "string" || fullName.trim() === "") {
+            throw new Error("Invalid full name: must be non-empty string.");
+        }
+        this._fullName = fullName;
+    }
+
+    /**
+     * Get the email address of the contact
+     * @returns {string}
+     */
+    get emailAddress() {
+        return this._emailAddress;
+    }
+
+    /**
+     * Set the email address of the contact. Validates input to ensure it's a standard email format
+     * @param emailAddress
+     */
+    set emailAddress(emailAddress) {
+        // const emailPattern = new RegExp(/^\w+@\w{3,}\.\w{2,4}$/);
+        const emailPattern = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+        if (!emailPattern.test(emailAddress)) {
+            throw new Error("Invalid email address: must be non-empty string.");
+        }
+        this._emailAddress = emailAddress;
+    }
+
+    /**
+     * Get the subject
+     * @returns {string}
+     */
+    get subject() {
+        return this._subject;
+    }
+
+    /**
+     * Set the subject. Validates input to ensure it's a non-empty string
+     * @param subject
+     */
+    set subject(subject) {
+        if (typeof subject !== "string" || subject.trim() === "") {
+            throw new Error("Invalid subject: must be non-empty string.");
+        }
+        this._subject = subject;
+    }
+
+    /**
+     * Get the message
+     * @returns {string}
+     */
+    get message() {
+        return this._message;
+    }
+
+    /**
+     * Set the message. Validates input to ensure it's a non-empty string
+     * @param message
+     */
+    set message(message) {
+        if (typeof message !== "string" || message.trim() === "") {
+            throw new Error("Invalid message: must be non-empty string.");
+        }
+        this._message = message;
+    }
+}
